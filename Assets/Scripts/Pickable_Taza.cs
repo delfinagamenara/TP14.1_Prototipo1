e979f360a3d48092cfa429e622ddfed9ed9d05ff
delfinagamenara <delfigamenara@gmail.com>
2026-05-27 14:18:17 -0300

    Clase 2: Mecánica de recolección funcional y modelo con materiales

---
+++ Assets/Scripts/Pickable_Taza.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pickable_Taza : MonoBehaviour
+{
+      private static int contador = 0;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+      private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("InteractiveArea"))
+        {
+       contador++;
+        Debug.Log("Tazas recolectadas: " + contador);
+            Destroy(gameObject);
+        }
+    }
+}
